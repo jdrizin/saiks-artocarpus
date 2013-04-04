c71920f2e04f173309ed9b079ab89e30ec098c05
Josh Drizin <josh@respectablename.com>
2013-04-04 14:18:59 -0500

    the script works, though has hardcoded paths

--- charconvert.py
+++ charconvert.py
@@ -2,6 +2,9 @@
 #this script should read in a few files and spit out a useful SAIKS js file
 #you can see an example on https://github.com/jdrizin/saiks
 
+#i'm using regexpn
+import re
+
 #read in the files and strip newlines
 coding = [line.strip() for line in open('/home/josh/artocarpus/saiks/charactercoding.csv')]
 states = [line.strip() for line in open('/home/josh/artocarpus/saiks/characterstates.csv')]
@@ -23,3 +26,17 @@ varitems = 'var items = [ [""],\n'
 #wrap the csv lines in [],\n;
 wcoding = ["[" + s + "],\n" for s in coding]
 wstates = ["[" + s + "],\n" for s in states]
+
+#replace ,\n in the last element with ];, the closing phrase
+wcoding[-1] = re.sub(',\\n', '];', wcoding[-1] )
+wstates[-1] = re.sub(',\\n', '];', wstates[-1] )
+
+#output everything
+output = (dataset + "\n" + varbin + "\n" + 
+		  varchars + ''.join(str(elem) for elem in wstates) + "\n" +
+		  varitems + ''.join(str(elem) for elem in wcoding))
+
+#write out a text file
+text_file = open("/home/josh/artocarpus/saiks/artocarpus.js", "w")
+text_file.write(output)
+text_file.close
